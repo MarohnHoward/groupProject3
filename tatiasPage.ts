04af4ac780be965d01705f6d7734ca104e95363c
Tatia Gujabidze <tatiagujabidze@gmail.com>
2024-09-19 21:42:12 -0400

    Login test

--- tatiasPage.ts
+++ tatiasPage.ts
@@ -4,9 +4,10 @@ const fs = require('fs')
 
 export class TatiasPage extends BasePage {
     noThanks: By = By.xpath('//div[text()="No, thanks"]');
-    iconBtn: By = By.xpath('(//img[@class="relative top-0 left-0 w-auto h-full fill-black max-h-[18px] max-w-[25px] xl:max-w-[38px] xl:max-h-[33px]"])[2]');
-    emailField: By = By.name('[name="customer[email]"]');
-    passwordField: By = By.name ('[name="customer[password]"]');
+    xBtn: By = By.xpath('//button[@class="system-btn close prompt-close-btn"]');
+    iconBtn: By = By.xpath('//div[@class="Header-icon Header-icon--account h-full flex flex-col justify-between items-center gap-0.5"]');
+    emailField: By = By.name('customer[email]');
+    passwordField: By = By.name ('customer[password]');
     signIn: By = By.xpath('//input[@class="w-full tracking-wider text-xs font-body font-bold uppercase cursor-pointer rounded-full transition-colors bg-black border-2 border-solid border-transparent text-center py-3 text-white hover:bg-[#5A5A5A] mt-5"]');
     signOut: By = By.xpath('//a[@class="js-logout bg-[#F8F3F5] ml-2 mt-10 flex items-center gap-1 p-0.5 pl-3 pr-4 text-sm text-black capitalize w-max rounded-full group hover:bg-black hover:text-white"]');
 
@@ -21,7 +22,7 @@ export class TatiasPage extends BasePage {
         .perform()};
 
     async tabSwitch() {
-        let muTabs = await this.driver.getAllWindowHandles();
+        let myTabs = await this.driver.getAllWindowHandles();
         await this.driver.switchTo().window(myTabs[1]);
         await this.driver.sleep(1000);
         fs.writeFile(`${__dirname}/DiamondArtClub.png`,
--- tatiasTest.test.ts
+++ tatiasTest.test.ts
@@ -1,13 +1,18 @@
+import { Driver } from "selenium-webdriver/chrome";
 import { TatiasPage } from "./tatiasPage";
 const page = new TatiasPage();
 
 test ('log in to the account', async () => {
     await page.navigate();
     await page.click(page.noThanks);
+    await page.driver.sleep(1000);
     await page.click(page.iconBtn);
     await page.driver.sleep(1000);
+    //await page.click(page.xBtn);
     await page.setInput(page.emailField, 'loveandboneandlove@91ail.com');
-    await page.setInput(page.passwordField, 'devmountain');
+    await page.setInput(page.passwordField, 'devmountaintest');
     await page.click(page.signIn);
-
+    await page.driver.sleep(1000);
+    await page.click(page.signOut);
+    await page.driver.quit();
 });
