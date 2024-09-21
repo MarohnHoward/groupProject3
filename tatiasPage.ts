8f5702091825e63e28857af05de378444381e508
Tatia Gujabidze <tatiagujabidze@gmail.com>
2024-09-20 23:10:13 -0400

    added three other tests

--- tatiasPage.ts
+++ tatiasPage.ts
@@ -10,6 +10,22 @@ export class TatiasPage extends BasePage {
     passwordField: By = By.name ('customer[password]');
     signIn: By = By.xpath('//input[@class="w-full tracking-wider text-xs font-body font-bold uppercase cursor-pointer rounded-full transition-colors bg-black border-2 border-solid border-transparent text-center py-3 text-white hover:bg-[#5A5A5A] mt-5"]');
     signOut: By = By.xpath('//a[@class="js-logout bg-[#F8F3F5] ml-2 mt-10 flex items-center gap-1 p-0.5 pl-3 pr-4 text-sm text-black capitalize w-max rounded-full group hover:bg-black hover:text-white"]');
+    shopAll: By = By.xpath('(//a[@class="flex items-center h-full !outline-none text-base font-bold leading-5 tracking-[0.04em] text-black hover:text-[rgb(179,136,235)]"])[1]');
+    orangeBlossom: By = By.xpath('//a[text()="Classic Orange Blossom"]');
+    arrowBtn: By = By.xpath('//div[@class="swiper-button-next"]');
+    addToCart: By = By.name('add');
+    header: By = By.xpath('//a[@class="pb-4 lg:pb-2 block"]');
+    searchBar: By = By.name('q');
+    //snowMan: By = By.xpath('(//span[@class="snize-thumbnail"])[7]');
+    viewProduct: By = By.xpath('(//button[text()="View product"])[3]');
+    yourEmailAddress: By = By.xpath('//input[@class="swym-input"]');
+    emailMe: By = By.id('swym-remind-email-auth-button');
+    takeAQuiz: By = By.xpath('//a[text()="Take a Quiz!"]');
+    startQuiz: By = By.id('start-quiz-btn');
+    natureLandscapes: By = By.xpath('(//li[@class="oct-quiz-multiple-choice-options__option"])[8]');
+    medium: By = By.xpath('(//li[@class="oct-quiz-multiple-choice-options__option"])[2]');
+    round: By = By.xpath('(//li[@class="oct-quiz-multiple-choice-options__option"])[1]');
+
 
     constructor () {
         super ({url: 'https://www.diamondartclub.com/'});
--- tatiasTest.test.ts
+++ tatiasTest.test.ts
@@ -14,5 +14,47 @@ test ('log in to the account', async () => {
     await page.click(page.signIn);
     await page.driver.sleep(1000);
     await page.click(page.signOut);
+});
+
+
+test ('add a product to the shopping cart', async () => {
+    await page.click(page.shopAll);
+    await page.click(page.orangeBlossom);
+    await page.driver.sleep(1000);
+    await page.click(page.arrowBtn);
+    await page.driver.sleep(1000);
+    await page.click(page.arrowBtn);
+    await page.driver.sleep(1000);
+    await page.click(page.arrowBtn);
+    await page.driver.sleep(1000);
+    await page.click(page.addToCart);
+    await page.driver.sleep(1000);
+    await page.click(page.header);
+    await page.driver.sleep(1000);
+   
+});
+
+test ('email me when back in stock', async () => {
+    await page.setInput(page.searchBar, 'Snow man\n');
+    //await page.click(page.snowMan);
+    await page.driver.sleep(1000);
+    await page.click(page.viewProduct);
+    await page.driver.sleep(1000);
+    await page.setInput(page.yourEmailAddress, 'abcdloeh@hijl.pnk');
+    await page.driver.sleep(1000);
+    await page.click(page.emailMe);
+    await page.driver.sleep(1000);
+});
+
+test ('take a quiz', async () => {
+    await page.click(page.takeAQuiz);
+    await page.click(page.startQuiz);
+    await page.driver.sleep(1000);
+    await page.click(page.natureLandscapes);
+    await page.driver.sleep(1000);
+    await page.click(page.medium);
+    await page.driver.sleep(1000);
+    await page.click(page.round);
+    await page.driver.sleep(1000);
     await page.driver.quit();
 });
